d077659b4adcf58b287c6a5bd9dd222d74ab4965
rizki113070213 <rizki113070213@gmail.com>
2018-10-20 13:33:06 +0700

    creating registration form

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from foodtaskerapp.forms import UserForm, RestaurantForm
 
 # Create your views here.
 def home(request):
@@ -10,4 +11,10 @@ def restaurant_home(request):
     return render(request, 'restaurant/home.html')
 
 def restaurant_sign_up(request):
-    return render(request, 'restaurant/sign_up.html', {})
+    user_form = UserForm()
+    restaurant_form = RestaurantForm()
+
+    return render(request, 'restaurant/sign_up.html', {
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
